8d588b23f2ae160443a96dfccb529993c8a09f9d
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-02-07 15:13:20 +0100

    Essai méthode andThen

--- src/TestFunction.java
+++ src/TestFunction.java
@@ -3,22 +3,41 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
+/*
+ * Essai de redefinition des méthodes d'interface fonctionnelle avec les lambdas
+ */
 public class TestFunction {
 
 	public static void main(String[] args) {
 
+		boolean variante = false;
+		
 		List<Personne> lPersonne = Arrays.asList(
 				new Personne(10,"Toto"),
 				new Personne(20,"Titi"),
 				new Personne(30,"Tata"),
 				new Personne(40,"Tutu"));
+
+			
+		if (variante) {
+			//redéfinition de la méthode de l'interface fonctionnelle Function<T, U>
+			Function<Personne, String> f1 = (Personne p) -> p.getNom();
+			Function<Personne, Integer> f2 = (Personne p) -> p.getAge() * 2;
+			
+			System.out.println(transformToListString(lPersonne, f1));
+			System.out.println(transformToListInt(lPersonne, f2));
+		}
+		else {
+			//redéfinition de la méthode de l'interface fonctionnelle Function<T, U>
+			Function<Personne, String> f1 = (Personne p) -> p.getNom();
+			Function<Personne, Integer> f2 = (Personne p) -> p.getAge();
+			Function<Integer, Integer> f3 = (Integer a) -> a * 2;
+			
+			System.out.println("Essai avec la méthode par défaut \"andThen\"");
+			System.out.println(transformToListString(lPersonne, f1));
+			System.out.println(transformToListInt(lPersonne, f2.andThen(f3)));
+		}
 		
-		//redéfinition de la méthode de l'interface fonctionnelle Function<T, U>
-		Function<Personne, String> f1 = (Personne p) -> p.getNom();
-		Function<Personne, Integer> f2 = (Personne p) -> p.getAge() * 2;
-		
-		System.out.println(transformToListString(lPersonne, f1));
-		System.out.println(transformToListInt(lPersonne, f2));
 		
 
 	}
